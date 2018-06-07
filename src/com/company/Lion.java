0be7e2b3a316fb47db38d793e0178b7419c4ae22
Christopher Jones <cmjones751@gmail.com>
2018-06-07 13:38:38 -0400

    Created a Rabbit, Lion, Rhino, and Penguin Class

---
+++ src/com/company/Lion.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class Lion extends Animal {
+
+    private double noseSize;
+    private double ManeLength;
+    private boolean isTrained;
+
+
+    public Lion(String name, String location, char gender, double weight, double maneLength, boolean isTrained) {
+        super(name, location, gender, weight);
+        ManeLength = maneLength;
+        this.isTrained = isTrained;
+    }
+
+
+    public double getNoseSize() {
+        return noseSize;
+    }
+
+    public void setNoseSize(double noseSize) {
+        this.noseSize = noseSize;
+    }
+
+    public double getManeLength() {
+        return ManeLength;
+    }
+
+    public void setManeLength(double maneLength) {
+        ManeLength = maneLength;
+    }
+
+    public boolean isTrained() {
+        return isTrained;
+    }
+
+    public void setTrained(boolean trained) {
+        isTrained = trained;
+    }
+}
