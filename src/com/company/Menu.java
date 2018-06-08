62ed6450f634b46e4a7b0922b096ab630c02593f
Christopher Jones <cmjones751@gmail.com>
2018-06-08 11:01:32 -0400

    Created Menu Class

---
+++ src/com/company/Menu.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Menu {
+
+    private Scanner input = new Scanner(System.in);
+
+    //void can be any data type/ doesn't return a value
+
+   public void mainMenu() {
+       System.out.println("Welcome to the Online Zoo!");
+
+       System.out.println("please choose an option. " +
+               "\n1. Manage People" +
+               "\n2. Manage Animals" +
+               "\n3. Manage inventory");
+
+       // handles user input for navigation
+   }
+
+
+
+}
